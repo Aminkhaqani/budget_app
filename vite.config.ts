d680044b6871edfabfea06b41991d7a289cfd8a6
Amin khaghani <90598644+Aminkhaqani@users.noreply.github.com>
2026-02-20 14:28:12 +0000

    Fix Vite base + ignore Vite cache

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "/budget_app/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/budget_app/" : "/",
   plugins: [react(), tailwindcss()],
-});
+}));
